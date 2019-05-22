65da5a6b586dbc79b4daa7fe1f44dc60f7dbcdca
zero2603 <20153713@student.hust.edu.vn>
2019-05-22 14:47:16 +0700

    web-mining-project

---
+++ movies/content_based.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+# import matplotlib.pyplot as plt
+
+# Reading ratings file
+# Ignore the timestamp column
+ratings = pd.read_csv('data/ratings.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'movie_id', 'rating'])
+
+# Reading users file
+users = pd.read_csv('data/users.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
+
+# Reading movies file
+movies = pd.read_csv('data/movies.csv', sep='\t', encoding='latin-1', usecols=['movie_id', 'title', 'genres'])
+
+# CONTENT BASED 
+# Break up the big genre string into a string array
+movies['genres'] = movies['genres'].str.split('|')
+# Convert genres to string value
+movies['genres'] = movies['genres'].fillna("").astype('str')
+
+tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(movies['genres'])
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+def recommend(movie_id):
+        sim_scores = list(enumerate(cosine_sim[movie_id]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[0:7]
+
+        movie_indices = []
+        for i in sim_scores: 
+                if movie_id != i[0]:
+                        movie_indices.append(i[0])
+
+        return movie_indices
---
+++ movies/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from django.template import loader
+import numpy as np
+import pandas as pd
+import content_based
+
+movies = np.array(pd.read_csv("data/movies.csv", sep='\t', usecols=['movie_id', 'title', 'genres']))
+
+def index(request):
+    template = loader.get_template('home.html')
+    user_id = 0
+
+    if request.session.get('user_id'):
+        user_id = request.session.get('user_id') 
+
+    context = {
+        'movies': movies[:20], 
+        'user_id': user_id,
+    }
+    return HttpResponse(template.render(context, request)) 
+
+def login(request):
+    template = loader.get_template('home.html')
+    request.session['user_id'] = request.POST['username']
+
+    with open("recommended/user/" + request.session['user_id'] + ".txt") as f:
+        recommended_movies_id = f.readlines()
+    recommended_movies_id = map(int, recommended_movies_id)
+    
+    recommended_movies = []
+    for item in movies:
+        if item[0] in recommended_movies_id:
+            recommended_movies.append(item)
+
+    context = {
+        'movies': movies[:20], 
+        'user_id': request.session['user_id'], 
+        'recommended_movies_1': recommended_movies[4:],
+        'recommended_movies_2': recommended_movies[:4],
+    }
+    return HttpResponse(template.render(context, request))
+
+def get_detail(request):
+    template = loader.get_template('detail.html')
+    movie_id = int(request.GET.get('movie'))
+    movie= None
+    
+    for item in movies:
+        if item[0] == movie_id:
+            movie = item
+            break
+
+    recommended_movies_id = content_based.recommend(movie_id)
+    recommended_movies = []
+    for item in movies:
+        if item[0] in recommended_movies_id:
+            recommended_movies.append(item)
+
+    context = {'movie': movie, 'recommended_movies': recommended_movies}
+    return HttpResponse(template.render(context, request))
